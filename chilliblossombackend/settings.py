8412ee5ffc6ad010f209ed8153744df6233500cb
Kittu14216 <kittumarisetti@gmail.com>
2024-11-25 23:54:10 +0530

    Initial commit

--- chilliblossombackend/settings.py
+++ chilliblossombackend/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 
